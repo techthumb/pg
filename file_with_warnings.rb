c6bcc99f18cf9b3e622b009f6cc04146fd88dd79
Kunal Parikh <kunal@techthumb.in>
2009-11-20 12:06:13 +1100

    first commit

---
+++ file_with_warnings.rb
@@ -0,0 +1,13 @@
+class A
+  def do_something with, lots, aand, pots, of, parameters
+    a = 1
+    b = 2
+    c = 3
+    d = 4
+    e = 5
+    g = 6
+    
+    a + b + c + d + e + f + g
+    a * b * c * d * e * f * g    
+  end
+end
